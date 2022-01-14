bfa4eb34b598c6d55f02a4e6685b08a6640d2b69
ergi98 <edervishaj98@gmail.com>
2022-01-14 15:08:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Socket
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
