c278d27435ffe8a467abccca4572ccfabb3e8b17
ergi98 <edervishaj98@gmail.com>
2022-01-14 21:47:16 +0100

    React hot reload only on development

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,14 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: "./public/index.html",
+  }),
+];
 
 let mode = "development";
 let target = "web";
@@ -10,6 +18,8 @@ let target = "web";
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   target = "browserslist";
+} else {
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 
 module.exports = {
@@ -18,17 +28,12 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx", ".tsx", ".ts"],
   },
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash]][ext][query]",
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+  plugins: plugins,
   module: {
     rules: [
       {
