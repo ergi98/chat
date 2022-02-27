535acff50e8eb6363f157393ffd5240514d774e2
ergi98 <edervishaj98@gmail.com>
2022-02-27 10:46:30 +0100

    Removed the automatic scroll to bottom fucntionality and added a button to do it

--- client/src/views/chat_room/ChatRoom.jsx
+++ client/src/views/chat_room/ChatRoom.jsx
@@ -1,24 +1,27 @@
-import React, { useRef, useEffect, useState, useCallback, useLayoutEffect } from 'react';
+import React, { useRef, useEffect, useState, useLayoutEffect } from 'react';
 import styles from './chat-room.module.css';
 
 // Components
 import Send from '../../components/chat_room/send/Send';
 import Chat from '../../components/chat_room/chat/Chat';
 import TopRibbon from '../../components/chat_room/top_ribbon/TopRibbon';
+import NoUsersLeftModal from '../../components/chat_room/no_users_modal/NoUsersLeftModal';
 
 // Context
 import { useRoot } from '../../RootContext';
 
 // Api
+import { getRoom } from '../../mongo/room';
 import { sendMessage, getMessagesByChunks } from '../../mongo/message.js';
 
 // UUID
 import { v4 } from 'uuid';
 
 // ANTD
-import { message } from 'antd';
-import { getRoom } from '../../mongo/room';
-import NoUsersLeftModal from '../../components/chat_room/no_users_modal/NoUsersLeftModal';
+import { Button, message } from 'antd';
+
+// Utilities
+import { throttle } from '../../utilities/general.utilities';
 
 async function fetchMessages(date = null) {
   try {
@@ -44,9 +47,12 @@ function ChatRoom() {
   const [hasMoreToFetch, setHasMoreToFetch] = useState(true);
   const [showNoUsersLeft, setShowNoUsersLeft] = useState(false);
 
+  const [showNewMessage, setShowNewMessage] = useState(false);
+
   const [hasSetListeners, setHasSetListeners] = useState(false);
   const [typingIndicator, setTypingIndicator] = useState(false);
 
+  // Socket listeners
   useEffect(() => {
     function handleNewMessage(message) {
       // If yours update
@@ -59,7 +65,7 @@ function ChatRoom() {
       }
       // If not yours append to end
       else setRoomMessages((previous) => [...previous, message]);
-      scrollToBottom();
+      isOverflowing(chatRef) && setShowNewMessage(true);
     }
 
     function handleNewMember(data) {
@@ -102,15 +108,15 @@ function ChatRoom() {
       rootData?.socket?.off('new-message');
       rootData?.socket?.off('finished-typing');
     };
-  }, [rootData.socket, rootData.user, scrollToBottom]);
+  }, [rootData.socket, rootData.user]);
 
+  // Resize Observer (When send field expands vertically)
   useLayoutEffect(() => {
     let observer,
       localSendRef = sendRef.current,
       localChatRef = chatRef.current;
 
     function attachResizeObserver(observerVar, expandingElRef, shrinkingElRef) {
-      console.log('%c ChatRoom - Resize observer', 'color: #bf55da');
       // Resize the chat depending on the input field height
       observerVar = new ResizeObserver((entries) => {
         for (let entry of entries) {
@@ -125,12 +131,12 @@ function ChatRoom() {
 
     return () => {
       if (localChatRef?.current && observer) {
-        console.log('%c  ChatRoom - Removing observer', 'background: red; color: #fefefe');
         observer?.unobserve(localSendRef);
       }
     };
   }, []);
 
+  // Global View Height (Mobile 100vh Fix)
   useLayoutEffect(() => {
     function setGlobalVh() {
       let vh = window.innerHeight * 0.01;
@@ -146,6 +152,7 @@ function ChatRoom() {
     };
   }, []);
 
+  // Initial Message Fetch
   useEffect(() => {
     async function initialFetch() {
       setLoading(true);
@@ -153,11 +160,12 @@ function ChatRoom() {
       setLastFetchDate(date);
       setRoomMessages((prev) => [...messages, ...prev]);
       setLoading(false);
-      scrollToBottom();
+      if (messages?.length && isOverflowing(chatRef)) setShowNewMessage(true);
     }
     hasSetListeners && initialFetch();
-  }, [hasSetListeners, scrollToBottom]);
+  }, [hasSetListeners]);
 
+  // Fetch Messages on top scroll
   // TODO:
   useEffect(() => {
     async function checkIfScrolledToTop(event) {
@@ -165,7 +173,6 @@ function ChatRoom() {
         if (hasMoreToFetch === false) return;
         setLoading(true);
         let { date, messages } = await fetchMessages(lastFetchDate);
-
         setLastFetchDate(date);
         setHasMoreToFetch(!!messages.length);
         setRoomMessages((prev) => [...messages, ...prev]);
@@ -191,6 +198,39 @@ function ChatRoom() {
       }
     };
   }, [chatRef, hasMoreToFetch, lastFetchDate]);
+
+  // Hide new message on bottom scroll
+  useEffect(() => {
+    function checkIfScrolledToBottom(event) {
+      let current = event.target.scrollHeight - event.target.scrollTop;
+      let max = event.target.clientHeight;
+      if (Math.abs(current - max) <= 10) {
+        setShowNewMessage(false);
+      }
+    }
+
+    function addScrollBottomListener() {
+      chatRef.current.addEventListener('scroll', throttle(checkIfScrolledToBottom, 500));
+    }
+
+    addScrollBottomListener();
+
+    return () => {
+      chatRef.current.removeEventListener('scroll', throttle(checkIfScrolledToBottom, 500));
+    };
+  }, []);
+
+  function isOverflowing(ref) {
+    let value = false;
+    // Is overflowing
+    if (ref.current.scrollHeight > ref.current.clientHeight) {
+      // Is not at the end
+      if (ref.current.scrollTop !== ref.current.scrollHeight) {
+        value = true;
+      }
+    }
+    return value;
+  }
 
   async function addNewMessage({ text, imageData, audio }) {
     let tempMessageId = v4();
@@ -230,18 +270,24 @@ function ChatRoom() {
     }
   }
 
-  const scrollToBottom = useCallback(() => {
-    endOfMessages?.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [endOfMessages]);
+  function scrollToBottom() {
+    endOfMessages.current.scrollIntoView({ behavior: 'smooth' });
+    setShowNewMessage(false);
+  }
 
   return (
     <div className={styles['chat-room']}>
       <TopRibbon />
       <Chat ref={chatRef} messages={roomMessages} loading={loading}>
-        {typingIndicator ? (
+        {typingIndicator && !showNewMessage ? (
           <div className={styles['typing-indicator']}>Friend is typing ...</div>
         ) : null}
         <div ref={endOfMessages}></div>
+        {showNewMessage ? (
+          <Button onClick={scrollToBottom} className={styles['new-message']} type="dashed">
+            New Message
+          </Button>
+        ) : null}
       </Chat>
       <Send ref={sendRef} addMessage={addNewMessage} scrollToBottom={scrollToBottom} />
       {showNoUsersLeft ? <NoUsersLeftModal show={showNoUsersLeft} /> : null}
